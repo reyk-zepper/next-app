5d1880666be522a17780e7281e48679e1cbdd622
Reyk Zepper <reyk.zepper@gmail.com>
2023-04-25 17:07:44 +0200

    implement SSG

---
+++ src/pages/user/index.js
@@ -0,0 +1,24 @@
+export default function index({ users }) {
+  return (
+    <div>
+      <h1>user list</h1>
+      {users.map((user) => (
+        <div key={user.id}>
+          <h3>{user.name}</h3>
+          <p>{user.email}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await res.json();
+
+  return {
+    props: {
+      users,
+    },
+  };
+}
